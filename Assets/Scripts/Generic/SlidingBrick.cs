3985fe91f322a2398dea5abee833f52bef89989c
Nathan <nathanielorozco42@gmail.com>
2025-10-15 09:48:50 -0500

    DoTweenAdded

---
+++ Assets/Scripts/Generic/SlidingBrick.cs
@@ -0,0 +1,11 @@
+using DG.Tweening;
+using UnityEngine;
+
+public class SlidingBrick : MonoBehaviour
+{
+    
+    void Start()
+    {
+        //transform.DOPunchScale(new Vector3(1.1f, 1.1f, 1.1f), 0.1f).SetLoops(3);
+    }
+}
